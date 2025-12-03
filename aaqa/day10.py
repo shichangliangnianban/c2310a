3862594fd822c4d3786be39d88053e819ed97350
shichangliangnianban <160831269+shichangliangnianban@users.noreply.github.com>
2025-12-03 16:10:21 +0800

    Delete aaqa directory

--- aaqa/day10.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = pd.read_csv("../assignment_part1 2/assignment_part1/submission.csv")
-y = data.iloc[:,:-1]
-print(y)
-class_0 = data[y==0]
-class_1 = data[y==1]
-print(len(class_0))
-print(len(class_1))
