3ab96c3d404ae3b799c714646d1889ee4fd61ad4
shichangliangnianban <160831269+shichangliangnianban@users.noreply.github.com>
2025-12-03 16:10:45 +0800

    Delete day16 directory

--- day16/word.py
+++
@@ -1,9 +0,0 @@
-import jieba
-from gensim.models import Word2Vec
-def f(a,b):
-    raw_sentence = [jieba.lcut("番茄非常好吃"), jieba.lcut("西红柿熟了"), jieba.lcut("我喜欢吃西红柿")]
-    model = Word2Vec(raw_sentence,vector_size=192,min_count=1)
-    return float(model.wv.similarity("西红柿", "番茄"))
-
-if __name__=="__main__":
-    print(f("番茄","西红柿"))
